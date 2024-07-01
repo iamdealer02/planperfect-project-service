0a6cc30dcf3ec38308967a9ac1d0fe9ac31e141d
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-01 12:53:13 +0200

    Feat: View functionality - Create a project functionality with models

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from .models import Project
+from projectService.utils.mongodb import get_mongo_db
+from bson.objectid import ObjectId
 
-# Create your views here.
+# Initialize MongoDB collection
+mongo_db = get_mongo_db()
+collection = mongo_db['projects']
+
+class ProjectViewSet(viewsets.ViewSet):
+    
+    def create(self, request):
+        data = request.data
+        required_fields = ['name', 'description', 'start_date', 'end_date', 'is_active']
+        for field in required_fields:
+            if field not in data:
+                return Response({'error': f'{field} is required'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            project = Project(**data)
+            collection.insert_one(project.to_dict())
+            return Response(project.to_dict(), status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': f'Failed to create project: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
+
+
