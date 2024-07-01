d77f5ff75d5e377dce14afbd0bd9b82e1d75ddec
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-02 00:16:52 +0200

    Model setup

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+# because this is Mongo model without orms we do not need models from django
 
-# Create your models here.
+class Team:
+    # add team member to ur project
+    def __init__(self, project_id, member_id, role):
+        self.project_id = project_id
+        self.member_id = member_id
+        self.role = role
+    
+    def to_dict(self):
+        return {
+            'project_id': self.project_id,
+            'member_id': self.member_id,
+            'role': self.role
+        }
+    
+    @staticmethod
+    def from_dict(data):
+        return Team(
+            project_id=data['project_id'],
+            member_id=data['member_id'],
+            role=data['role']
+        )
+    
